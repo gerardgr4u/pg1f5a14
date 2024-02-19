f73b4d1fa5f2fb45b443a3a129d281379f23eb98
gerardgr4u <15584495.clot@fje.edu>
2024-02-19 19:15:54 +0000

    Fase C.1

--- app/index1.php
+++ app/index1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Predicció Premier League</title>
+    <title>Predicció Premier League 1.0</title>
 </head>
 <body>
     <h1>Partits de la pròxima jornada</h1>
